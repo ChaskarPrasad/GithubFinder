ac314f75c09305c494db458e407eade2f55ec533
Chaskar <prasadbhausaheb.chaskar@solera.com>
2023-03-13 14:21:16 +0530

    Removed Github token

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+// const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
 const github = axios.create({
   baseURL: GITHUB_URL,
-  headers: { Authorization: `token ${GITHUB_TOKEN}` },
+  //headers: { Authorization: `token ${GITHUB_TOKEN}` },
 })
 
 // Get search results
-export const searchUsers = async (text) => {
+export const searchUsers = async (text) => { 
   const params = new URLSearchParams({
     q: text,
   })
